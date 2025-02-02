02ebb03a23c4b8fa721be075c112abf8675a9efb
PutuRifkiDy <puturifki56@gmail.com>
2025-02-02 20:50:51 +0800

    fix: ui create proposal form

--- resources/js/Pages/Proposals/Partials/CreateProposalForm.jsx
+++ resources/js/Pages/Proposals/Partials/CreateProposalForm.jsx
@@ -2,6 +2,11 @@ import { useParam } from "@/utils";
 import { DocumentArrowUpIcon } from "@heroicons/react/24/outline";
 import { DocumentIcon } from "@heroicons/react/24/solid";
 import { useForm } from "@inertiajs/react";
+import {
+    CheckCircleIcon,
+    ExclamationCircleIcon,
+    ExclamationTriangleIcon,
+} from "@heroicons/react/24/solid";
 
 export default function CreateProposalForm({ user }) {
     const { data, setData, post, processing, errors, recentlySuccessful } =
@@ -19,62 +24,69 @@ export default function CreateProposalForm({ user }) {
     const schemes = ["PKM-GFT", "PKM-K", "PKM-KC", "PKM-PI", "PKM-PM"];
 
     return (
-        <div className="lg:w-1/2">
-            <DocumentIcon className="h-10 w-10 mb-4" />
-            <h3 className="font-bold text-xs mb-4">PROPOSAL</h3>
+        <div className="bg-white flex gap-5 flex-col px-10 py-10 rounded-[14px]">
+            {/* <DocumentIcon className="h-10 w-10 mb-4" /> */}
             <form onSubmit={submit}>
                 {/* Input Title */}
-                <div className="form-control my-2">
-                    <label htmlFor="title" className="font-bold mb-2">
-                        Judul
-                    </label>
-                    <textarea
-                        id="title"
-                        name="title"
-                        value={data.title}
-                        rows={2}
-                        autoComplete="title"
-                        isfocused="true"
-                        onChange={(e) => setData("title", e.target.value)}
-                        className="textarea textarea-bordered"
-                    ></textarea>
+                <div className="grid grid-cols-1 gap-2">
 
-                    <p className="mt-2 text-error">{errors.title}</p>
-                </div>
 
-                {/* Select PKM Scheme */}
-                <div className="form-control my-2">
-                    <label htmlFor="title" className="font-bold mb-2">
-                        Skema
-                    </label>
-                    <select
-                        id="scheme"
-                        name="scheme"
-                        className="select select-bordered w-full text-base"
-                        value={data.scheme}
-                        onChange={(e) => setData("scheme", e.target.value)}
-                    >
-                        <option value="" disabled>
-                            Pilih skema PKM
-                        </option>
-                        {schemes.map((scheme, i) => (
-                            <option key={i} value={scheme}>
-                                {scheme}
+                    {/* Select PKM Scheme */}
+                    <div className="form-control my-2">
+                        <div className={`w-full px-5 py-3.5 flex flex-row gap-5 items-center bg-[#357F8B]/10 border-l-4 border-[#357F8B] rounded-l-[4px] mb-10`}>
+                            <ExclamationCircleIcon className="h-6 w-6 fill-[#357F8B]" />
+                            <p className="font-bold text-[14px] leading-[16px] text-[#357F8B]">
+                                Masukan Skema dan Judul Proposal Pada Form Di bawah ini
+                            </p>
+                        </div>
+                        <label htmlFor="title" className="font-bold mb-2">
+                            Skema
+                        </label>
+                        <select
+                            id="scheme"
+                            name="scheme"
+                            className="select select-bordered w-full text-base"
+                            value={data.scheme}
+                            onChange={(e) => setData("scheme", e.target.value)}
+                        >
+                            <option value="" disabled>
+                                Pilih skema PKM
                             </option>
-                        ))}
-                    </select>
+                            {schemes.map((scheme, i) => (
+                                <option key={i} value={scheme}>
+                                    {scheme}
+                                </option>
+                            ))}
+                        </select>
 
-                    <p className="mt-2 text-error">{errors.scheme}</p>
+                        <p className="mt-2 text-error">{errors.scheme}</p>
+                    </div>
+
+                    <div className="form-control my-2">
+                        <label htmlFor="title" className="font-bold mb-2">
+                            Judul
+                        </label>
+                        <textarea
+                            id="title"
+                            name="title"
+                            value={data.title}
+                            rows={2}
+                            autoComplete="title"
+                            isfocused="true"
+                            onChange={(e) => setData("title", e.target.value)}
+                            className="textarea textarea-bordered"
+                        ></textarea>
+
+                        <p className="mt-2 text-error">{errors.title}</p>
+                    </div>
                 </div>
-
-                <button
-                    className="btn btn-warning mb-2 w-full"
-                    disabled={processing}
-                    type="submit"
-                >
-                    <DocumentArrowUpIcon className="h-6 w-6" />
-                    Simpan Proposal
-                </button>
+                    <button
+                        className="flex flex-row justify-center items-center font-bold bg-[#42A1A4] px-10 py-2 text-[18px tracking-[0.03em] leading-[26px] rounded-md text-white hover:text-white hover:bg-[#59DFD1] dark:text-gray-400 dark:hover:text-white transition-all duration-300 shadow-[0_0_10px_#42A1A4]"
+                        disabled={processing}
+                        type="submit"
+                    >
+                        Simpan Proposal
+                    </button>
             </form>
         </div>
     );
