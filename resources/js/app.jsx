09ea3e2588cea9c5ce47542f55fba20bba183888
PutuRifkiDy <puturifki56@gmail.com>
2025-02-11 10:46:57 +0800

    update

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -8,7 +8,7 @@ import { PrimeReactProvider } from "primereact/api";
 import Tailwind from "primereact/passthrough/tailwind";
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css'; 
+import 'primeicons/primeicons.css';
 import { twMerge } from 'tailwind-merge';
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
