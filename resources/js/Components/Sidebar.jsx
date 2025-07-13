348285509dc65ad6e40af49200c6a5f16f59c1f5
tridarma6 <triidarma06@gmail.com>
2025-07-13 21:12:28 +0800

    update: tambah role dosen di web pkm ti ( tampilin page proposal aja di admin)

--- resources/js/Components/Sidebar.jsx
+++ resources/js/Components/Sidebar.jsx
@@ -204,6 +204,30 @@ export default function Sidebar({ user, navigations, children }) {
                                                 </Link>
                                             </li>
                                         ))}
+                                        {user.role === "lecturer" &&
+                                            (location.pathname.search(/admin/) === -1 ? (
+                                                <li className="border-b-2 border-slate-200 dark:border-slate-600">
+                                                    <Link
+                                                        href={route("admin.proposals")} // Assuming a lecture dashboard route
+                                                        as="button" 
+                                                        className="flex flex-row gap-2 justify-start items-center font-medium text-[14px] light:text-[#404040] dark:text-white tracking-[0.11em]"
+                                                    >
+                                                        <AcademicCapIcon className="h-6 w-6" /> {/* You might want a different icon */}
+                                                        Mode Lecture
+                                                    </Link>
+                                                </li>
+                                            ) : (
+                                                <li className="border-b-2 border-slate-200 dark:border-slate-600">
+                                                    <Link
+                                                        href={route("dashboard")}
+                                                        as="button"
+                                                        className="flex flex-row gap-2 justify-start items-center font-medium text-[14px] light:text-[#404040] dark:text-white tracking-[0.11em]"
+                                                    >
+                                                        <AcademicCapIcon className="h-6 w-6" />
+                                                        Mode Participant
+                                                    </Link>
+                                                </li>
+                                            ))}
                                     <li>
                                         <a
                                             onClick={() =>
