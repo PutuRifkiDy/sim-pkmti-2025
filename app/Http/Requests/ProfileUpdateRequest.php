e8cb7d674a6c01cdb32f666fcbe787ecc1c93837
PutuRifkiDy <puturifki56@gmail.com>
2025-08-09 23:07:36 +0800

    Update: All

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Requests;
 
 use App\Models\User;
@@ -17,10 +16,10 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
-            'nim' => ['required', 'string', 'min:10', 'max:10', Rule::unique(User::class)->ignore($this->user()->id)],
-            'phone' => ['required', 'regex:/^08[0-9]+$/'],
+            'name'    => ['required', 'string', 'max:255'],
+            'email'   => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'nim'     => ['required', 'string', 'min:10', 'max:10', Rule::unique(User::class)->ignore($this->user()->id)],
+            'phone'   => ['required', 'regex:/^08[0-9]+$/'],
             'line_id' => ['required', 'string'],
         ];
     }
